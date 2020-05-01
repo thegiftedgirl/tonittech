6d70ce60dca9c6bf4ffe5b90f591201071d31f8b
Toni Horne <tonihorne@Tonis-MacBook-Air.local>
2020-04-30 23:19:29 -0400

    Add resume.js file and downloadable link

---
+++ src/pages/resume.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from'../components/Layout'
+
+
+export default () => {
+return (
+    <Layout>
+
+    <h1> My Resume </h1>
+    <a href="https://docdro.id/YbJUAui" target="__blank">
+   download resume
+    </a>
+    </Layout>
+)
+}
