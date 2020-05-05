abe2da20b72fea2c53537369a6f9b6d2f1363e30
Toni Horne <tonihorne@Tonis-MacBook-Air.local>
2020-05-05 17:34:56 -0400

    Add blog page and link to nav

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+
+export default () => {
+    return(
+        <h1>
+            Blog
+            <Link to="/contact">Contact</Link>
+        </h1>
+    )
+}
